7871a3136f997e011e1550e5826cccfb4ef70e01
codes-swalih <codesswalih@gmail.com>
2026-02-17 10:31:31 +0530

    update student database

--- components/Verificationscomp/Verificationscomp.js
+++ components/Verificationscomp/Verificationscomp.js
@@ -94,6 +94,350 @@ const Verificationscomp = () => {
       certNumber: "cbpd/idft/25/3106",
       learnerNumber: "cpl5499/002",
     },
+
+    // --- Newly Added Students ---
+
+    {
+      name: "sruthy p",
+      regNumber: "CBPD/11029",
+      certNumber: "CBPD/ISDME/25/3109",
+      learnerNumber: "CPL5499/005",
+    },
+    {
+      name: "alice nelson",
+      regNumber: "CBPD/11030",
+      certNumber: "CBPD/ISDME/25/3110",
+      learnerNumber: "CPL5499/006",
+    },
+    {
+      name: "salma joseph",
+      regNumber: "CBPD/11031",
+      certNumber: "CBPD/ISDME/25/3111",
+      learnerNumber: "CPL5499/007",
+    },
+    {
+      name: "abhirami s",
+      regNumber: "CBPD/11032",
+      certNumber: "CBPD/ISDME/25/3112",
+      learnerNumber: "CPL5499/008",
+    },
+    {
+      name: "saranya b",
+      regNumber: "CBPD/11033",
+      certNumber: "CBPD/ISDME/25/3113",
+      learnerNumber: "CPL5499/009",
+    },
+    {
+      name: "manju s",
+      regNumber: "CBPD/11034",
+      certNumber: "CBPD/ISDME/25/3114",
+      learnerNumber: "CPL5499/010",
+    },
+    {
+      name: "anzila i",
+      regNumber: "CBPD/11035",
+      certNumber: "CBPD/ISDME/25/3115",
+      learnerNumber: "CPL5499/011",
+    },
+    {
+      name: "akhila mol u",
+      regNumber: "CBPD/11036",
+      certNumber: "CBPD/ISDME/25/3116",
+      learnerNumber: "CPL5499/012",
+    },
+    {
+      name: "arya ajayakumar",
+      regNumber: "CBPD/11037",
+      certNumber: "CBPD/ISDME/25/3117",
+      learnerNumber: "CPL5499/013",
+    },
+    {
+      name: "haneesha s",
+      regNumber: "CBPD/11038",
+      certNumber: "CBPD/ISDME/25/3118",
+      learnerNumber: "CPL5499/014",
+    },
+
+    {
+      name: "jolly raju",
+      regNumber: "CBPD/11039",
+      certNumber: "CBPD/ISDME/25/3119",
+      learnerNumber: "CPL5499/015",
+    },
+    {
+      name: "amina kk",
+      regNumber: "CBPD/11040",
+      certNumber: "CBPD/ISDME/25/3120",
+      learnerNumber: "CPL5499/016",
+    },
+    {
+      name: "gopika thirumeni",
+      regNumber: "CBPD/11041",
+      certNumber: "CBPD/ISDME/25/3121",
+      learnerNumber: "CPL5499/017",
+    },
+    {
+      name: "geethu rajendran",
+      regNumber: "CBPD/11042",
+      certNumber: "CBPD/ISDME/25/3122",
+      learnerNumber: "CPL5499/018",
+    },
+    {
+      name: "rajalekshmi r",
+      regNumber: "CBPD/11043",
+      certNumber: "CBPD/ISDME/25/3123",
+      learnerNumber: "CPL5499/019",
+    },
+    {
+      name: "surya cs",
+      regNumber: "CBPD/11044",
+      certNumber: "CBPD/ISDME/25/3124",
+      learnerNumber: "CPL5499/020",
+    },
+    {
+      name: "akshara anil",
+      regNumber: "CBPD/11045",
+      certNumber: "CBPD/ISDME/25/3125",
+      learnerNumber: "CPL5499/021",
+    },
+    {
+      name: "rajalakshmi s",
+      regNumber: "CBPD/11046",
+      certNumber: "CBPD/ISDME/25/3126",
+      learnerNumber: "CPL5499/022",
+    },
+    {
+      name: "aswathy majesh",
+      regNumber: "CBPD/11047",
+      certNumber: "CBPD/ISDME/25/3127",
+      learnerNumber: "CPL5499/023",
+    },
+    {
+      name: "najiya f",
+      regNumber: "CBPD/11048",
+      certNumber: "CBPD/ISDME/25/3128",
+      learnerNumber: "CPL5499/024",
+    },
+
+    {
+      name: "telma rose",
+      regNumber: "CBPD/11049",
+      certNumber: "CBPD/ISDME/25/3129",
+      learnerNumber: "CPL5499/025",
+    },
+    {
+      name: "sneha t",
+      regNumber: "CBPD/11050",
+      certNumber: "CBPD/ISDME/25/3130",
+      learnerNumber: "CPL5499/026",
+    },
+    {
+      name: "ramsheena p.a",
+      regNumber: "CBPD/11051",
+      certNumber: "CBPD/ISDME/25/3131",
+      learnerNumber: "CPL5499/027",
+    },
+    {
+      name: "steniya saji",
+      regNumber: "CBPD/11052",
+      certNumber: "CBPD/ISDME/25/3132",
+      learnerNumber: "CPL5499/028",
+    },
+    {
+      name: "neethu k sidhan",
+      regNumber: "CBPD/11053",
+      certNumber: "CBPD/ISDME/25/3133",
+      learnerNumber: "CPL5499/029",
+    },
+    {
+      name: "thasniya k.m",
+      regNumber: "CBPD/11054",
+      certNumber: "CBPD/ISDME/25/3134",
+      learnerNumber: "CPL5499/030",
+    },
+    {
+      name: "steffy devassy",
+      regNumber: "CBPD/11055",
+      certNumber: "CBPD/ISDME/25/3135",
+      learnerNumber: "CPL5499/031",
+    },
+    {
+      name: "swapna george",
+      regNumber: "CBPD/11056",
+      certNumber: "CBPD/ISDME/25/3136",
+      learnerNumber: "CPL5499/032",
+    },
+    {
+      name: "preethy saji",
+      regNumber: "CBPD/11057",
+      certNumber: "CBPD/ISDME/25/3137",
+      learnerNumber: "CPL5499/033",
+    },
+    {
+      name: "resmi rajesh",
+      regNumber: "CBPD/11058",
+      certNumber: "CBPD/ISDME/25/3138",
+      learnerNumber: "CPL5499/034",
+    },
+    {
+      name: "bhuvaneshwari p",
+      regNumber: "CBPD/11059",
+      certNumber: "CBPD/ISDME/25/3139",
+      learnerNumber: "CPL5499/035",
+    },
+
+    {
+      name: "jasnath mol t.p",
+      regNumber: "CBPD/11060",
+      certNumber: "CBPD/ISDME/25/3140",
+      learnerNumber: "CPL5499/036",
+    },
+    {
+      name: "jaseela jasmin",
+      regNumber: "CBPD/11061",
+      certNumber: "CBPD/ISDME/25/3141",
+      learnerNumber: "CPL5499/037",
+    },
+    {
+      name: "fathima v.t",
+      regNumber: "CBPD/11062",
+      certNumber: "CBPD/ISDME/25/3142",
+      learnerNumber: "CPL5499/038",
+    },
+    {
+      name: "jasmin n",
+      regNumber: "CBPD/11063",
+      certNumber: "CBPD/ISDME/25/3143",
+      learnerNumber: "CPL5499/039",
+    },
+    {
+      name: "shaji mol m",
+      regNumber: "CBPD/11064",
+      certNumber: "CBPD/ISDME/25/3144",
+      learnerNumber: "CPL5499/040",
+    },
+
+    {
+      name: "radhika l",
+      regNumber: "CBPD/11065",
+      certNumber: "CBPD/IASDME/25/3145",
+      learnerNumber: "CPL5499/041",
+    },
+    {
+      name: "drishya shajesh",
+      regNumber: "CBPD/11066",
+      certNumber: "CBPD/IASDME/25/3146",
+      learnerNumber: "CPL5499/042",
+    },
+    {
+      name: "seethu thomas",
+      regNumber: "CBPD/11067",
+      certNumber: "CBPD/IASDME/25/3147",
+      learnerNumber: "CPL5499/043",
+    },
+    {
+      name: "sabitha s",
+      regNumber: "CBPD/11068",
+      certNumber: "CBPD/IASDME/25/3148",
+      learnerNumber: "CPL5499/044",
+    },
+    {
+      name: "nadiya nr",
+      regNumber: "CBPD/11069",
+      certNumber: "CBPD/IASDME/25/3149",
+      learnerNumber: "CPL5499/045",
+    },
+    {
+      name: "thresia silvester",
+      regNumber: "CBPD/11070",
+      certNumber: "CBPD/IASDME/25/3150",
+      learnerNumber: "CPL5499/046",
+    },
+    {
+      name: "akshaya",
+      regNumber: "CBPD/11071",
+      certNumber: "CBPD/IASDME/25/3151",
+      learnerNumber: "CPL5499/047",
+    },
+    {
+      name: "surya sunil",
+      regNumber: "CBPD/11072",
+      certNumber: "CBPD/IASDME/25/3152",
+      learnerNumber: "CPL5499/048",
+    },
+    {
+      name: "sreelekshmi a",
+      regNumber: "CBPD/11073",
+      certNumber: "CBPD/IASDME/25/3153",
+      learnerNumber: "CPL5499/049",
+    },
+    {
+      name: "aneena mary",
+      regNumber: "CBPD/11074",
+      certNumber: "CBPD/IASDME/25/3154",
+      learnerNumber: "CPL5499/050",
+    },
+
+    {
+      name: "sandhya vc",
+      regNumber: "CBPD/11075",
+      certNumber: "CBPD/IASDME/25/3155",
+      learnerNumber: "CPL5499/051",
+    },
+    {
+      name: "greeshma s",
+      regNumber: "CBPD/11076",
+      certNumber: "CBPD/IASDME/25/3156",
+      learnerNumber: "CPL5499/052",
+    },
+    {
+      name: "sherin ks",
+      regNumber: "CBPD/11077",
+      certNumber: "CBPD/IASDME/25/3157",
+      learnerNumber: "CPL5499/053",
+    },
+    {
+      name: "anjaly kv",
+      regNumber: "CBPD/11078",
+      certNumber: "CBPD/IASDME/25/3158",
+      learnerNumber: "CPL5499/054",
+    },
+    {
+      name: "bhavyalakshmi s",
+      regNumber: "CBPD/11079",
+      certNumber: "CBPD/IASDME/25/3159",
+      learnerNumber: "CPL5499/055",
+    },
+    {
+      name: "shamily v.a",
+      regNumber: "CBPD/11080",
+      certNumber: "CBPD/IASDME/25/3160",
+      learnerNumber: "CPL5499/056",
+    },
+    {
+      name: "ashitha ramesh",
+      regNumber: "CBPD/11081",
+      certNumber: "CBPD/IASDME/25/3161",
+      learnerNumber: "CPL5499/057",
+    },
+    {
+      name: "mariya sunny",
+      regNumber: "CBPD/11082",
+      certNumber: "CBPD/IASDME/25/3162",
+      learnerNumber: "CPL5499/058",
+    },
+    {
+      name: "athira a.r",
+      regNumber: "CBPD/11083",
+      certNumber: "CBPD/IASDME/25/3163",
+      learnerNumber: "CPL5499/059",
+    },
+    {
+      name: "nisha k mony",
+      regNumber: "CBPD/11084",
+      certNumber: "CBPD/IASDME/25/3164",
+      learnerNumber: "CPL5499/060",
+    },
   ];
 
   const handleInputChange = (e) => {
@@ -134,7 +478,7 @@ const Verificationscomp = () => {
         normalizeString(student.name) === normalizedStudentName &&
         normalizeString(student.regNumber) === normalizedRegNumber &&
         normalizeString(student.certNumber) === normalizedCertNumber &&
-        normalizeString(student.learnerNumber) === normalizedLearnerNumber
+        normalizeString(student.learnerNumber) === normalizedLearnerNumber,
     );
 
     if (matchedStudent) {
@@ -166,7 +510,7 @@ const Verificationscomp = () => {
       console.log("Calling searchMemberships API...");
       const response = await searchMemberships(
         memberName.trim(),
-        memberNumber.trim()
+        memberNumber.trim(),
       );
       console.log("API response:", response);
 
@@ -208,7 +552,7 @@ const Verificationscomp = () => {
       console.log("Calling searchCenters API...");
       const response = await searchCenters(
         centreName.trim(),
-        centreCode.trim()
+        centreCode.trim(),
       );
       console.log("API response:", response);
 
